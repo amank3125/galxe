a4c7cf0721bf461371fda9b49262f51d85133019
Aman <amank3125@gmail.com>
2024-03-02 00:09:44 +0530

    console resp data

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ const fetchData = async () => {
 
     const data = await response.json();
     h1.innerText = `GraphQL Response: ${data}`;
-    console.log(data);
+    console.log(Object.keys(data));
   } catch (error) {
     h1.innerText = `Error fetching data: ${error}`;
   }
